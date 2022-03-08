2b94849188fd560df2d36e7f132bc711d73f27ce
dhi1ka <daw.knight04@gmail.com>
2022-03-08 14:26:26 +0700

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,6 @@ import { IoMdAdd, IoMdSearch } from "react-icons/io";
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
 
-  if (!user) return <Link to="login">Login</Link>;
-
   return (
     <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
       <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
@@ -21,21 +19,25 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
         />
       </div>
 
-      <div className="flex gap-3">
-        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-          <img
-            src={user.image}
-            alt="userImage"
-            className="w-14 h-12 rounded-lg"
-          />
-        </Link>
-        <Link
-          to="create-pin"
-          className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
-        >
-          <IoMdAdd />
-        </Link>
-      </div>
+      {user ? (
+        <div className="flex gap-3">
+          <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+            <img
+              src={user.image}
+              alt="userImage"
+              className="w-14 h-12 rounded-lg"
+            />
+          </Link>
+          <Link
+            to="create-pin"
+            className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
+          >
+            <IoMdAdd />
+          </Link>
+        </div>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
     </div>
   );
 };
